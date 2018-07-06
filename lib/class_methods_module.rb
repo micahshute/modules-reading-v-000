519159f5f966008ac526e38e953903f495db2c21
Micah Shute <micah.shute@gmail.com>
2018-07-06 03:26:42 +0000

    automatically backed up by learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,3 +1,10 @@
+module MetaDancing
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+
+
 module ClassMethods
   def metadata
     "This class produces objects that love to dance."
